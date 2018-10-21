7cd66a10e1ba13e26786f227e7b6f6accaaa77c4
MatiasBaci <matibaciga@gmail.com>
2018-10-21 03:59:34 -0300

    Update datos_prueba.py

--- datos_prueba.py
+++ datos_prueba.py
@@ -23,12 +23,11 @@ def cargar_datos_prueba():
     giorno_giovanna = ["contraseña", "Giorno", "Giovanna", "hombre", 18, (0, 0), ["helado", "italia", "taxis", "robar"]]
     jotaro_kujo = ["contraseña", "Jotaro", "Kujo", "hombre", 28, (0, 0), ["delfines", "fumar", "poker"]]
     lisa_lisa = ["contraseña", "Lisa", "Lisa", "mujer", 36, (0, 0), ["baño-de-inmersion", "bufandas", "lentes"]]
-    suzi_q = ["contraseña", "", "", "", , (, ), ["fotografia", "japon", "bromas", ""]]
-    joseph_joestar = ["contraseña", "", "", "", , (, ), ["disfraces", "armas", "nueva-york", ""]]
-    dio_brando = ["contraseña", "", "", "", , (, ), ["sangre", "poder", "gatos", "relojes", "cuchillos"]]
-    erina_pendleton = ["contraseña", "", "", "", , (, ), ["medicina", "", "", ""]]
-
-
-    diccionario = {"juan_perez": juan_perez, "giorno_giovanna": giorno_giovanna, "jotaro_kujo": jotaro_kujo, "lisa_lisa": lisa_lisa, "suzi_q": suzi_q, "joseph_joestar": joseph_joestar, "dio_brando": dio_brando, "erina_pendleton": erina_pendleton, , }
-
+    suzi_q = ["contraseña", "Suzi", "Q", "mujer", 50, (, ), ["fotografia", "japon", "bromas", ""]]
+    joseph_joestar = ["contraseña", "Joseph", "Joestar", "hombre", 50, (, ), ["disfraces", "armas", "nueva-york", ""]]
+    dio_brando = ["contraseña", "Dio", "Brando", "hombre", 99, (, ), ["sangre", "poder", "gatos", "relojes", "cuchillos"]]
+    erina_pendleton = ["contraseña", "Erina", "Pendleton", "mujer", 18, (, ), ["medicina", "", "", ""]]
+    jane_doe = ["contraseña", "Jane", "Doe", "mujer", 25, (, ), ["asado", "musica", "green-day", "star-wars"]]
+    john_doe = ["contraseña", "John", "Doe", "hombre", 25, (, ), ["bicicleta", "taekwondo", "buenos-aires", "basquet"]]
+    diccionario = {"juan_perez": juan_perez, "giorno_giovanna": giorno_giovanna, "jotaro_kujo": jotaro_kujo, "lisa_lisa": lisa_lisa, "suzi_q": suzi_q, "joseph_joestar": joseph_joestar, "dio_brando": dio_brando, "erina_pendleton": erina_pendleton, "jane_doe": jane_doe, "john_doe": john_doe}
     return diccionario
