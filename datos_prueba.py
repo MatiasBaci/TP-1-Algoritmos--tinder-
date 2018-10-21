818bdfffc9aebd09ecb38740cb8ff419b81707e1
TomBPotochek <TomBPotochek@users.noreply.github.com>
2018-10-21 01:50:25 -0300

    acomodar el coso ese
    que ce yo lmao gotem

--- datos_prueba.py
+++ datos_prueba.py
@@ -1,21 +1,22 @@
 #Diccionario con los usuarios de testeo
 
-#- Nombre
-#- Apellido
+
 #- Pseudónimo (único en el sistema, solo minúsculas, números y guión bajo)
 #- Contraseña (requiere al menos una mayúscula, una minúscula, un dígito decimal, y un
 #largo mínimo de 5 caracteres)
+#- Nombre
+#- Apellido
 #- Sexo
-#- Indefinido
-#- Mujer
-#- Hombre
+#   - Indefinido
+#   - Mujer
+#   - Hombre
 #- Edad (de 18 a 99)
 #- Ubicación actual: Latitud y longitud en grados decimales (ejemplo: 41.40338, 2.17403)
 #- Intereses
-#- Los intereses son una lista de etiquetas (palabras sin espacios ni acentos, unidas
-#con guiones medios) a ser ingresadas por los usuarios. Por ejemplo: basquet,
-#green-day, star-wars, nueva-york, fotografia, francia, asado, bicicleta, taekwondo,
-#buenos-aires.
+#   - Los intereses son una lista de etiquetas (palabras sin espacios ni acentos, unidas
+#   con guiones medios) a ser ingresadas por los usuarios. Por ejemplo: basquet,
+#   green-day, star-wars, nueva-york, fotografia, francia, asado, bicicleta, taekwondo,
+#   buenos-aires.
 
 Juan_Perez = ["1235","Juan","Perez","hombre",34,(1,2),["sushi","roirros","troirros"]]
 
