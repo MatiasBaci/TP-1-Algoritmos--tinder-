b0f74cd3696f3979bba4a14ddabf405ccf171b80
TomBPotochek <TomBPotochek@users.noreply.github.com>
2018-10-21 23:48:02 -0300

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -37,32 +37,74 @@ def nuevo_pseudonimo(dicc_usuarios):
     while enProceso:        ###para ver si el proceso de elegir pseudonimo esta terminado o en proceso
         pseudonimo = input("Ingrese su pseudonimo, compuesto unicamente de minusculas, numeros y guiones bajos\nPseudonimo: ")
         if pseudonimo in dicc_usuarios:
-            print("el pseudónimo elegido ya esta en uso. Por favor eliga otro")
+            print("El pseudónimo elegido ya esta en uso. Por favor eliga otro")
         elif not esValidoPseudonimo(pseudonimo):        #Entra a esta parte si el pseudonimo no es valido
-            print("El pseudónimo solo puede contener minusculas, números y guiones bajo '_'")
+            print("El pseudónimo solo puede contener minusculas, números y guiones bajos '_'")
         else:
             enProceso = False
     return pseudonimo
 
 
 def esValidoPseudonimo(string_de_caracteres):           #Chequea que el pseudonimo no contenga caracteres no permitidos
-    caracteres_permitidos = (*"abcdefghijklmnñopqrstuvwxyz1234567890_",)         #Tupla conteniendo los carateres permitidos en forma empaquetada
-    tupla_del_string = (*string_de_caracteres,)         #Lo mismo que con la tupla 'caracteres_permitidos' pero con el pseudonimo
-    for caracter in tupla_del_string:
+    caracteres_permitidos = (*"abcdefghijklmnopqrstuvwxyz1234567890_",)         #Tupla conteniendo los carateres permitidos en forma empaquetada
+    for caracter in string_de_caracteres:
         if caracter not in caracteres_permitidos:
             return False
     return True
 
+
+
 def password():
 
+
+
 def sex():                      ###dejar estas funciones menores para el final
+    sexo_valido = False
+    ##salir = False
+    caracteres_permitidos = ("h", "m", "i", "hombre", "mujer", "indefinido")
+    while not sexo_valido: ##or not salir:
+        sexo = input("Ingrese sexo\n'h' hombre\n'm' mujer\n'i' indefinido\n's' salir\nSexo:").lower()
+        if sexo in caracteres_permitidos:
+            sexo_valido = True
+        else:
+            print("Invalido")
+    if sexo not in ("hombre", "mujer", "indefinido"):
+        if sexo == "h":
+            sexo = "hombre"
+        elif sexo == "m":
+            sexo = "mujer"
+        elif sexo == "i":
+            sexo = "indefinido"
+    return sexo
+
 
 def age():
 
 def location():
 
 def interests():
+    otro_mas = True
+    intereses = []
+    while otro_mas:
+        interes = input("Ingrese un interes, o 'exit'\n").lower().strip()
+        interes = interes.replace(" ", "-")
+        if interes != "exit":
+            if es_valido_interes(interes, intereses):
+                intereses.append(interes)
+                print("{} ha sido agregado a sus intereses".format(interes))
+            else:
+                print("Caracteres invalidos, o el interes ya se encuentra en su lista de intereses")
+        else: otro_mas = False
+    return intereses
 
+def es_valido_interes(interes, intereses):
+    caracteres_validos = (*"abcdefghijklmnopqrstuvwxyz1234567890-",)
+    for caracter in interes:
+        if caracter not in caracteres_validos:  #se fija que sea valido
+            return False
+    if interes in intereses:    #se fija que no este repetido
+        return False
+    return True
 
 
 
@@ -97,6 +139,7 @@ def ingresar(dicc):
     return None,False
 
     
+def editar():        ###OPCIONAL
 
 
 
@@ -121,8 +164,6 @@ def findMatch(dicc_busqueda):       ###le das el diccionario con el usuario que 
 
 
 
-
-
 #Bloque principal
 
 
@@ -135,7 +176,7 @@ while opcion_usuario == 0:       #ciclo que ejecuta la funcion adecuada segun la
     if opcion_usuario == "1":
         diccionario_usuarios_prueba = cargar_datos_prueba()        ###hay que hacer un append para un diccionario mayor, y no permitir que se cargue varias veces
         print("Los datos han sido cargados")
-        opcion_usuario = 0
+        opcion_usuario = "0"
     elif opcion_usuario == "2":
         usuario_nuevo = registro()
         diccionario_usuarios_nuevos.update(usuario_nuevo)
