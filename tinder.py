98e3b3eed70ab718fd6b0059915fe3c0b95e2072
TomBPotochek <TomBPotochek@users.noreply.github.com>
2018-10-23 01:19:24 -0300

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -143,7 +143,8 @@ def location():
             print("oopsie whoopsie no ingresaste un número. Por favor ingresa un número UwU")
         else:
             lon_valido = True
-    return (latitud,longitud)
+    ubicacion = (latitud,longitud)
+    return ubicacion
 
 
 def interests():
@@ -238,7 +239,7 @@ def porcentaje_match(dicc_matches, lista_busqueda):      ### debe mostrar los us
         round(porcentaje)
         nombre = dicc_matches[match][1]
         apellido = dicc_matches[match][2]
-        print("Match!!! OwO <3 {nombre} {apellido} y vos tienen un {porcentaje}% de intereses en comun.")   ###si falla poner .format
+        print("Match!!! OwO <3 {} {} y vos tienen un {}% de intereses en comun.".format(nombre,apellido,porcentaje))   ###si falla poner .format
         ###aca deberiamos hacer que pregunte si quiere mandar un mensaje si fueron matcheados ambos
     print("Este porcentaje es completamente eficaz y para nada arbitrario a la hora de juzgar cuanto se parecen dos personas.")
     print("No, cuantificar la personalidad de alguien y reducirlo a un porcentaje no es absurdo.")
