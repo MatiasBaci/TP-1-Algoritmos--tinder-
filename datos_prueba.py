01f078140ed60790e091f8e0a2b2d58170b1077a
MatiasBaci <matibaciga@gmail.com>
2018-10-23 03:54:24 -0300

    hax para intereses
    Hice que todos tengan "hax" como interes, para facilitar testing. Si pones "hax" te deberian saltar todos

--- datos_prueba.py
+++ datos_prueba.py
@@ -19,18 +19,18 @@
 #   buenos-aires.
 
 def cargar_datos_prueba():
-    juan_perez = ["1235JoJo", "Juan", "Perez", "hombre", 34, (1, 2), ["sushi", "roirros", "troirros"]]
-    giorno_giovanna = ["contraseña", "Giorno", "Giovanna", "hombre", 18, (0, 0), ["helado", "italia", "taxis", "robar", "oro", "animales", "plantas"]]
-    jotaro_kujo = ["contraseña", "Jotaro", "Kujo", "hombre", 28, (0, 0), ["delfines", "fumar", "poker"]]
-    lisa_lisa = ["contraseña", "Lisa", "Lisa", "mujer", 36, (0, 0), ["baño-de-inmersion", "bufandas", "lentes"]]
-    suzi_q = ["contraseña", "Suzi", "Q", "mujer", 50, (0, 0), ["fotografia", "japon", "bromas"]]
-    joseph_joestar = ["contraseña", "Joseph", "Joestar", "hombre", 50, (50, 10), ["disfraces", "armas", "nueva-york"]]
-    dio_brando = ["contraseña", "Dio", "Brando", "hombre", 99, (0, 0), ["sangre", "poder", "gatos", "relojes", "cuchillos"]]
-    erina_obacha = ["contraseña", "Erina", "Pendleton", "mujer", 18, (10, 6), ["medicina", "barro"]]
-    jane_doe = ["contraseña", "Jane", "Doe", "mujer", 25, (0, 0), ["asado", "musica", "green-day", "star-wars"]]
-    john_doe = ["contraseña", "John", "Doe", "hombre", 25, (0, 0), ["bicicleta", "taekwondo", "buenos-aires", "basquet"]]
-    kakyoin = ["contraseña", "Noriaki", "Kakyoin", "hombre", 18, (95, 1), ["chupetines", "cerezas", "esmeraldas", "verde", "lentes"]]
-    von_stroheim = ["contraseña", "Rudol", "Von Stroheim", "hombre", 27, (0, 0), ["SS", "alemania", "mechs"]]
+    juan_perez = ["1235JoJo", "Juan", "Perez", "hombre", 34, (1, 2), ["hax", "sushi", "roirros", "troirros"]]
+    giorno_giovanna = ["contraseña", "Giorno", "Giovanna", "hombre", 18, (0, 0), ["hax", "helado", "italia", "taxis", "robar", "oro", "animales", "plantas"]]
+    jotaro_kujo = ["contraseña", "Jotaro", "Kujo", "hombre", 28, (0, 0), ["hax", "delfines", "fumar", "poker"]]
+    lisa_lisa = ["contraseña", "Lisa", "Lisa", "mujer", 36, (0, 0), ["hax", "baño-de-inmersion", "bufandas", "lentes"]]
+    suzi_q = ["contraseña", "Suzi", "Q", "mujer", 50, (0, 0), ["hax", "fotografia", "japon", "bromas"]]
+    joseph_joestar = ["contraseña", "Joseph", "Joestar", "hombre", 50, (50, 10), ["hax", "disfraces", "armas", "nueva-york"]]
+    dio_brando = ["contraseña", "Dio", "Brando", "hombre", 99, (0, 0), ["hax", "sangre", "poder", "gatos", "relojes", "cuchillos"]]
+    erina_obacha = ["contraseña", "Erina", "Pendleton", "mujer", 18, (10, 6), ["hax", "medicina", "barro"]]
+    jane_doe = ["contraseña", "Jane", "Doe", "mujer", 25, (0, 0), ["hax", "asado", "musica", "green-day", "star-wars"]]
+    john_doe = ["contraseña", "John", "Doe", "hombre", 25, (0, 0), ["hax", "bicicleta", "taekwondo", "buenos-aires", "basquet"]]
+    kakyoin = ["contraseña", "Noriaki", "Kakyoin", "hombre", 18, (95, 1), ["hax", "chupetines", "cerezas", "esmeraldas", "verde", "lentes"]]
+    von_stroheim = ["contraseña", "Rudol", "Von Stroheim", "hombre", 27, (0, 0), ["hax", "SS", "alemania", "mechs"]]
     diccionario = {"juan_perez": juan_perez, "giorno_giovanna": giorno_giovanna, "jotaro_kujo": jotaro_kujo, "lisa_lisa": lisa_lisa, "suzi_q": suzi_q, "joseph_joestar": joseph_joestar, "dio_brando": dio_brando, "erina_obacha": erina_obacha, "jane_doe": jane_doe, "john_doe": john_doe, "kakyoin": kakyoin, "von_stroheim": von_stroheim}
     return diccionario
 
