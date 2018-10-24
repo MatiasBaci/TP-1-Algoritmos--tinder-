9094f9ce944aefce46dbf23ffeb545fbf2705d3c
MatiasBaci <matibaciga@gmail.com>
2018-10-24 01:01:32 -0300

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -2,7 +2,7 @@
 ###uso 1 numeral (#) para cuando el mensaje es para los profes
 
 from datos_prueba import cargar_datos_prueba
-#from geopy.distance import geodesic
+from geopy.distance import geodesic
 import time
 
 
@@ -218,11 +218,14 @@ def busqueda(pseudonimo):       ### devuelve los datos para hacer la busqueda en
     print("Sexo en el que esta interesade\n")
     time.sleep(1)
     sexo_buscar = sex()
-    print("Rango de edades en las que esta interesade (la edad maxima de busqueda)\n")
     time.sleep(2)
-    rango_edad = age()
+    #rango_edad = age()
+    print("Edad minima de busqueda\n")
+    edad_min = age()
+    print("Edad maxima de busqueda\n")
+    edad_max = age()
     rango_distancia = float(input("Rango de busqueda\nIngrese el rango máximo de busqueda en kilómetros, puede ser decimal\n>"))
-    lista = [pseudonimo, [sexo_buscar, rango_edad, rango_distancia]]
+    lista = [pseudonimo, [sexo_buscar, (edad_min, edad_max), rango_distancia]]
     return lista
 
 
@@ -235,7 +238,8 @@ def findMatch(dicc_usuarios, lista_busqueda):       ###le das el diccionario con
         edad_max = lista_busqueda[1][1][1]
         sexo_interesado = lista_busqueda[1][0]
         distancia_al_usuario = geodesic(dicc_usuarios[usuario][5], info_usuario[5])
-        if (edad_min <= dicc_usuarios[usuario][4] <= edad_max) and (dicc_usuarios[usuario][3] == sexo_interesado) and (distancia_al_usuario <= lista_busqueda[0][2]):
+        #if (edad_min <= dicc_usuarios[usuario][4] <= edad_max) and (dicc_usuarios[usuario][3] == sexo_interesado) and (distancia_al_usuario <= lista_busqueda[0][2]):
+        if (edad_min <= dicc_usuarios[usuario][4] <= edad_max) and (dicc_usuarios[usuario][3] == sexo_interesado) and (distancia_al_usuario <= lista_busqueda[1][2]):
             datos = dicc_usuarios.pop(usuario)
             dicc_matches.update({usuario:datos})
     if dicc_matches == {}:
@@ -291,8 +295,10 @@ while opcion_usuario == "0":       #ciclo que ejecuta la funcion adecuada segun 
     elif opcion_usuario == "3":
         pseudonimoIngresado, valido = ingresar(diccionario_usuarios)
         if valido:
-            lista_busqueda = busqueda(diccionario_usuarios)
+            #lista_busqueda = busqueda(diccionario_usuarios)
             lista_busqueda = busqueda(pseudonimoIngresado)
+            dicc_matches, lista_busqueda = findMatch(diccionario_usuarios, lista_busqueda)
+            porcentaje_match(dicc_matches, lista_busqueda)
         opcion_usuario = "0"
     elif opcion_usuario == "4":
         print("Editar? No hay presupuesto para tantas funcionalidades.")
